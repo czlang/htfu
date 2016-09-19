1d0187ed9c56e9ace241db1c10ac96ad1a02fa65
Peter Láng <cz.lang@gmail.com>
2016-09-19 16:35:34 +0200

    added conformity init schema and some test data

--- src/htfu/component/datomic.clj
+++ src/htfu/component/datomic.clj
@@ -1,14 +1,21 @@
 (ns htfu.component.datomic
   (:require [com.stuartsierra.component :as component]
-            [datomic.api :as d]))
+            [datomic.api :as d]
+            [io.rkn.conformity :as c]
+            [taoensso.timbre :as timbre]))
 
 (defrecord Datomic [uri conn]
   component/Lifecycle
   (start [component]
     (let [db (d/create-database uri)
-          conn (d/connect uri)]
+          conn (d/connect uri)
+          norms-map (c/read-resource "htfu/htfu_schema.edn")]
+      (timbre/info "Obtained datomic connection" conn)
+      (c/ensure-conforms conn norms-map)
+      (timbre/info "Conformity finished")
       (assoc component :spec conn)))
   (stop [component]
+    (timbre/info "Discarding datomic connection")
     (assoc component :spec nil)))
 
 (defn datomic [uri]
